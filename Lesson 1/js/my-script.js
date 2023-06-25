bf0a84ed863c7d934997dd12c8391e0629092b54
saraT <taras_savenko@icloud.com>
2023-06-26 00:21:11 +0200

    myhomework1

---
+++ Lesson 1/js/my-script.js
@@ -0,0 +1,35 @@
+// Завдання №1
+
+let productQuantity = Number(prompt('Введіть кількість товарів'));
+let productPrice = 5.75;
+let summ = productQuantity * productPrice;
+console.log('Сума до сплати: ', summ);
+alert('Сума до сплати:' + (summ));
+
+// Завдання №2
+
+let productprice = Number(prompt('Введіть ціну'));
+let discount = productprice * 0.1;
+let Summ = productprice - discount;
+console.log('Ціна зі знижкою: ', Summ);
+alert('Ціна з вашим промокодом =' + (Summ));
+
+// Завдання №3
+
+let middleOne = Number(prompt('Введіть перше число'));
+let middleTwo = Number(prompt('Введіть друге число'));
+let middleThree = Number(prompt('Введіть третє число'));
+let middleFour = Number(prompt('Введіть четверте число'));
+let middle = ( middleOne + middleTwo + middleThree + middleFour) / 4;
+console.log('Середнє арифметичне значення =', middle);
+alert('Середнє рифметичне значення =' + middle);
+
+
+
+
+
+
+
+
+
+
